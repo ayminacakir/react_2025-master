ef6cfb8656d3b4d12890896dae2e76ebe2c6dd21
Aymina Çakır <114817398+ayminacakir@users.noreply.github.com>
2025-02-21 12:02:32 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -11,29 +12,8 @@ function App() {
 
   return (
     <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      ></ExpenseItem>
+      <Expenses expenses = {expenses}></Expenses>
+      
     </div>
   );
 }
---
+++ src/components/Expenses.jsx
@@ -0,0 +1,38 @@
+
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "./Card";
+
+function Expenses(expenses){
+    return(
+< Card className="expenses">
+      <ExpenseItem
+        title={expenses.items[0].title}
+        date={expenses.items[0].date}
+        amount={expenses.items[0].amount}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses.items[1].title}
+        date={expenses.items[1].date}
+        amount={expenses.items[1].amount}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses.items[2].title}
+        date={expenses.items[2].date}
+        amount={expenses.items[2].amount}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={expenses.items[3].title}
+        date={expenses.items[3].date}
+        amount={expenses.items[3].amount}
+      ></ExpenseItem>
+
+
+    </Card>
+
+    );
+}
+export default Expenses;
